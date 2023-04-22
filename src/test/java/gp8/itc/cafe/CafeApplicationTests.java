b28d894aaf8ab0ad47c629342197b4bdf87ffe1b
Korn-Monit <kornmonit@gmail.com>
2023-04-22 09:54:45 +0700

    v1

---
+++ src/test/java/gp8/itc/cafe/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package gp8.itc.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
